6a602cc22a061f3b873c5a17b0d44a26074d161e
Adam Dishaw <aldishaw@gmail.com>
2016-06-28 16:01:07 -0700

    Reduce output and skip already compiled tex files

--- run/texify.py
+++ run/texify.py
@@ -6,32 +6,68 @@ import tempfile
 import shutil
 import subprocess
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Compiles all .tex documents in a directory")
-    parser.add_argument("-i", "--input", default="tables", help="Directory with .tex files to compile")
-    parser.add_argument("-o", "--output", default="", help="Directory in which to put resulting .pdf files")
-    args = parser.parse_args()
+def Touch(file_path):
+    with open(file_path, 'a'):
+        os.utime(file_path, None)
 
+def FullPath(path):
+    return os.path.abspath(os.path.expanduser(path))
+
+def Texify(input_dirs, output_dir):
     orig_dir = os.getcwd()
 
-    in_dir = os.path.abspath(os.path.expanduser(args.input))
-    out_dir = ""
-    if args.output == "":
-        out_dir = in_dir
-    else:
-        out_dir = os.path.abspath(os.path.expanduser(args.output))
+    in_dirs = frozenset([FullPath(d) for d in input_dirs if os.path.exists(d)])
 
-    tmp_dir = tempfile.mkdtemp()
+    for raw_in_dir in in_dirs:
+        in_dir = FullPath(raw_in_dir)
+        out_dir = ""
+        if output_dir == None:
+            out_dir = in_dir
+        else:
+            out_dir = FullPath(output_dir)
+            if not os.path.exists(out_dir):
+                os.makedirs(out_dir)
+        tmp_dir = FullPath(tempfile.mkdtemp(prefix="tmp_texify_", dir=out_dir))
 
-    files = [f for f in os.listdir(in_dir) if f.endswith(".tex")]
-    for f in files:
-        os.symlink(os.path.join(in_dir, f), os.path.join(tmp_dir, f))
-        os.chdir(tmp_dir)
-        subprocess.call(["pdflatex","--shell-escape",f])
-        subprocess.call(["pdflatex","--shell-escape",f])
-        subprocess.call(["pdflatex","--shell-escape",f])
-        os.chdir(orig_dir)
-        the_pdf = f.replace(".tex",".pdf")
-        os.rename(os.path.join(tmp_dir, the_pdf), os.path.join(out_dir, the_pdf))
+        files = [f for f in os.listdir(in_dir) if f.endswith(".tex")]
+        for f in files:
+            the_pdf = f.replace(".tex",".pdf")
+            the_log = f.replace(".tex",".log")
+            tmp_pdf = os.path.join(tmp_dir, the_pdf)
+            out_pdf = os.path.join(out_dir, the_pdf)
+            tmp_log = os.path.join(tmp_dir, the_log)
+            out_log = os.path.join(out_dir, the_log)
+            in_tex = os.path.join(in_dir, f)
 
+            if os.path.exists(out_pdf) and os.path.getmtime(in_tex) < os.path.getmtime(out_pdf):
+                print("Kept pre-existing "+out_pdf)
+                continue
+            elif os.path.exists(out_log) and os.path.getmtime(in_tex) < os.path.getmtime(out_log):
+                print("Ignoring uncompilable "+in_tex)
+                continue
+
+            os.symlink(os.path.join(in_dir, f), os.path.join(tmp_dir, f))
+            os.chdir(tmp_dir)
+            command = ["pdflatex","--shell-escape","--interaction","batchmode",f]
+            null_file = open(os.devnull, 'w')
+            subprocess.call(command, stdout=null_file)
+            if os.path.exists(tmp_pdf):
+                subprocess.call(command, stdout=null_file)
+                subprocess.call(command, stdout=null_file)
+                os.rename(tmp_pdf, out_pdf)
+                print("Produced "+out_pdf)
+            else:
+                os.rename(tmp_log, out_log)
+                print("Failed to compile "+in_tex)
+
+    os.chdir(orig_dir)
     shutil.rmtree(tmp_dir)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Compiles all .tex documents in a set of directories",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-o", "--output", default=None, metavar="OUTPUT_DIR", help="Directory in which to put resulting .pdf files. Uses input directories if omitted.")
+    parser.add_argument("input", nargs="*", default=["tables"], metavar="INPUT_DIRS", help="List of directories with .tex files to compile")
+    args = parser.parse_args()
+
+    Texify(args.input, args.output)
