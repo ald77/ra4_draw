6efb0c717589b8cc72cbc0c6877c1214c9e90e48
Adam Dishaw <aldishaw@gmail.com>
2016-06-29 16:42:59 -0700

    Add delta R plots

--- inc/plot_dilep_angles.hpp
+++ inc/plot_dilep_angles.hpp
@@ -23,12 +23,16 @@ bool IsGoodMuon(const Baby &b, std::size_t imu);
 bool IsGoodElectron(const Baby &b, std::size_t iel);
 bool IsGoodTrack(const Baby &b, std::size_t itk);
 bool IsGoodJet(const Baby &b, std::size_t ijet);
-void GetAngles(const Baby &b, double &phi1, double &phi2);
+void GetAngles(const Baby &b,
+	       double &phi1, double &eta1,
+	       double &phi2, double &eta2);
 NamedFunc::ScalarType MinDeltaPhiLepMet(const Baby &b);
 NamedFunc::ScalarType MaxDeltaPhiLepMet(const Baby &b);
 NamedFunc::ScalarType MinDeltaPhiLepJet(const Baby &b);
 NamedFunc::ScalarType MaxDeltaPhiLepJet(const Baby &b);
 NamedFunc::ScalarType MinDeltaPhiMetJet(const Baby &b);
 NamedFunc::ScalarType MaxDeltaPhiMetJet(const Baby &b);
+NamedFunc::ScalarType MinDeltaRLepJet(const Baby &b);
+NamedFunc::ScalarType MaxDeltaRLepJet(const Baby &b);
 
 #endif
--- src/plot_dilep_angles.cxx
+++ src/plot_dilep_angles.cxx
@@ -42,7 +42,7 @@ int main(){
     "pass&&(trig[4]||trig[8]||trig[13]||trig[33])");
   auto data_2015 = Proc<Baby_full>("2015 Data", Process::Type::data, kRed,
     {"/net/cms2/cms2r0/babymaker/babies/2016_04_29/data/merged_1lht500met200/*.root"},
-    "pass&&(trig[4]||trig[8]||trig[13])");
+    "pass&&(trig[4]||trig[8]||trig[13]||trig[28])");
 
   vector<shared_ptr<Process> > procs = {data_2016, data_2015, tt1l, tt2l, wjets, single_t, ttv, other};
 
@@ -65,9 +65,9 @@ int main(){
   NamedFunc dilep_ev = baseline && "nels==1&&nmus==0&&nveto==1&&njets>=5&&nbm>=1&&mt>140";
   NamedFunc dilep_mv = baseline && "nels==0&&nmus==1&&nveto==1&&njets>=5&&nbm>=1&&mt>140";
 
-  NamedFunc dphi_ll("dphi_ll", [](const Baby &b){
-      double phi1, phi2;
-      GetAngles(b, phi1, phi2);
+  NamedFunc dphi_ll("dphi_ll", [](const Baby &b) -> NamedFunc::ScalarType{
+      double phi1, eta1, phi2, eta2;
+      GetAngles(b, phi1, eta1, phi2, eta2);
       if(phi1 == -999 || phi2 == -999) return -1.;
       return fabs(TVector2::Phi_mpi_pi(phi2-phi1));
     });
@@ -78,19 +78,33 @@ int main(){
   NamedFunc min_dphi_metj("min_dphi_metj", MinDeltaPhiMetJet);
   NamedFunc max_dphi_metj("max_dphi_metj", MaxDeltaPhiMetJet);
 
+  NamedFunc dr_ll("dr_ll", [](const Baby &b) -> NamedFunc::ScalarType{
+      double phi1, eta1, phi2, eta2;
+      GetAngles(b, phi1, eta1, phi2, eta2);
+      if(phi1 == -999 || phi2 == -999) return -1.;
+      return hypot(TVector2::Phi_mpi_pi(phi2-phi1), eta2-eta1);
+    });
+  NamedFunc min_dr_lj("min_dr_lj", MinDeltaRLepJet);
+  NamedFunc max_dr_lj("max_dr_lj", MaxDeltaRLepJet);
+
   vector<pair<string, NamedFunc> > categories = {{"all", dilep_all}, {"ee", dilep_ee}, {"em", dilep_em},
                                                  {"mm", dilep_mm}, {"ev", dilep_ev}, {"mv", dilep_mv}};
 
   for(const auto &cat: categories){
     std::string tag = "angles_"+cat.first;
     const NamedFunc &cut = cat.second;
-    pm.Push<HistoStack>(HistoDef(tag, 31, 0., pi, dphi_ll, "#Delta#phi(l,l)", cut), procs, plot_types);
-    pm.Push<HistoStack>(HistoDef(tag, 31, 0., pi, min_dphi_lmet, "Min. #Delta#phi(l,MET)", cut), procs, plot_types);
-    pm.Push<HistoStack>(HistoDef(tag, 31, 0., pi, max_dphi_lmet, "Max. #Delta#phi(l,MET)", cut), procs, plot_types);
-    pm.Push<HistoStack>(HistoDef(tag, 31, 0., pi, min_dphi_lj, "Min. #Delta#phi(l,jet)", cut), procs, plot_types);
-    pm.Push<HistoStack>(HistoDef(tag, 31, 0., pi, max_dphi_lj, "Max. #Delta#phi(l,jet)", cut), procs, plot_types);
-    pm.Push<HistoStack>(HistoDef(tag, 31, 0., pi, min_dphi_metj, "Min. #Delta#phi(MET,jet)", cut), procs, plot_types);
-    pm.Push<HistoStack>(HistoDef(tag, 31, 0., pi, max_dphi_metj, "Max. #Delta#phi(MET,jet)", cut), procs, plot_types);
+
+    pm.Push<HistoStack>(HistoDef(tag, 20, 0., pi, dphi_ll, "#Delta#phi(l,l)", cut), procs, plot_types);
+    pm.Push<HistoStack>(HistoDef(tag, 20, 0., pi, min_dphi_lmet, "Min. #Delta#phi(l,MET)", cut), procs, plot_types);
+    pm.Push<HistoStack>(HistoDef(tag, 20, 0., pi, max_dphi_lmet, "Max. #Delta#phi(l,MET)", cut), procs, plot_types);
+    pm.Push<HistoStack>(HistoDef(tag, 20, 0., pi, min_dphi_lj, "Min. #Delta#phi(l,jet)", cut), procs, plot_types);
+    pm.Push<HistoStack>(HistoDef(tag, 20, 0., pi, max_dphi_lj, "Max. #Delta#phi(l,jet)", cut), procs, plot_types);
+    pm.Push<HistoStack>(HistoDef(tag, 20, 0., pi, min_dphi_metj, "Min. #Delta#phi(MET,jet)", cut), procs, plot_types);
+    pm.Push<HistoStack>(HistoDef(tag, 20, 0., pi, max_dphi_metj, "Max. #Delta#phi(MET,jet)", cut), procs, plot_types);
+
+    pm.Push<HistoStack>(HistoDef(tag, 20, 0., 6, dr_ll, "#Delta R(l,l)", cut), procs, plot_types);
+    pm.Push<HistoStack>(HistoDef(tag, 20, 0., 6, min_dr_lj, "Min. #Delta R(l,jet)", cut), procs, plot_types);
+    pm.Push<HistoStack>(HistoDef(tag, 20, 0., 6, max_dr_lj, "Max. #Delta R(l,jet)", cut), procs, plot_types);
   }
 
   //pm.multithreaded_ = false;
@@ -137,18 +151,22 @@ bool IsGoodJet(const Baby &b, size_t ijet){
     && !b.jets_islep()->at(ijet);
 }
 
-void GetAngles(const Baby &b, double &phi1, double &phi2){
-  phi1 = -999.;
-  phi2 = -999.;
+void GetAngles(const Baby &b,
+	       double &phi1, double &eta1,
+	       double &phi2, double &eta2){
+  phi1 = -999.; eta1 = -999.;
+  phi2 = -999.; eta2 = -999.;
   bool h1=false, h2=false;
   if(b.nels()==2 && b.nmus()==0){
     for(size_t iel = 0; iel < b.els_pt()->size() && !(h1&&h2); ++iel){
       if(IsGoodElectron(b, iel)){
         if(!h1){
           phi1 = b.els_phi()->at(iel);
+          eta1 = b.els_sceta()->at(iel);
           h1 = true;
         }else if(!h2){
           phi2 = b.els_phi()->at(iel);
+	  eta2 = b.els_sceta()->at(iel);
           h2 = true;
         }
       }
@@ -158,9 +176,11 @@ void GetAngles(const Baby &b, double &phi1, double &phi2){
       if(IsGoodElectron(b, iel)){
         if(!h1){
           phi1 = b.els_phi()->at(iel);
+          eta1 = b.els_sceta()->at(iel);
           h1 = true;
         }else if(!h2){
           phi2 = b.els_phi()->at(iel);
+          eta2 = b.els_sceta()->at(iel);
           h2 = true;
         }
       }
@@ -169,9 +189,11 @@ void GetAngles(const Baby &b, double &phi1, double &phi2){
       if(IsGoodMuon(b, imu)){
         if(!h1){
           phi1 = b.mus_phi()->at(imu);
+          eta1 = b.mus_eta()->at(imu);
           h1 = true;
         }else if(!h2){
           phi2 = b.mus_phi()->at(imu);
+	  eta2 = b.mus_eta()->at(imu);
           h2 = true;
         }
       }
@@ -181,9 +203,11 @@ void GetAngles(const Baby &b, double &phi1, double &phi2){
       if(IsGoodMuon(b, imu)){
         if(!h1){
           phi1 = b.mus_phi()->at(imu);
+          eta1 = b.mus_eta()->at(imu);
           h1 = true;
         }else if(!h2){
           phi2 = b.mus_phi()->at(imu);
+          eta2 = b.mus_eta()->at(imu);
           h2 = true;
         }
       }
@@ -193,9 +217,11 @@ void GetAngles(const Baby &b, double &phi1, double &phi2){
       if(IsGoodElectron(b, iel)){
         if(!h1){
           phi1 = b.els_phi()->at(iel);
+          eta1 = b.els_sceta()->at(iel);
           h1 = true;
         }else if(!h2){
           phi2 = b.els_phi()->at(iel);
+          eta2 = b.els_sceta()->at(iel);
           h2 = true;
         }
       }
@@ -204,9 +230,11 @@ void GetAngles(const Baby &b, double &phi1, double &phi2){
       if(IsGoodTrack(b, itk)){
         if(!h1){
           phi1 = b.tks_phi()->at(itk);
+          eta1 = b.tks_eta()->at(itk);
           h1 = true;
         }else if(!h2){
           phi2 = b.tks_phi()->at(itk);
+          eta2 = b.tks_eta()->at(itk);
           h2 = true;
         }
       }
@@ -216,9 +244,11 @@ void GetAngles(const Baby &b, double &phi1, double &phi2){
       if(IsGoodMuon(b, imu)){
         if(!h1){
           phi1 = b.mus_phi()->at(imu);
+          eta1 = b.mus_eta()->at(imu);
           h1 = true;
         }else if(!h2){
           phi2 = b.mus_phi()->at(imu);
+          eta2 = b.mus_eta()->at(imu);
           h2 = true;
         }
       }
@@ -227,9 +257,11 @@ void GetAngles(const Baby &b, double &phi1, double &phi2){
       if(IsGoodTrack(b, itk)){
         if(!h1){
           phi1 = b.tks_phi()->at(itk);
+          eta1 = b.tks_eta()->at(itk);
           h1 = true;
         }else if(!h2){
           phi2 = b.tks_phi()->at(itk);
+          eta2 = b.tks_eta()->at(itk);
           h2 = true;
         }
       }
@@ -238,8 +270,8 @@ void GetAngles(const Baby &b, double &phi1, double &phi2){
 }
 
 NamedFunc::ScalarType MinDeltaPhiLepMet(const Baby &b){
-  double phi1, phi2;
-  GetAngles(b, phi1, phi2);
+  double phi1, eta1, phi2, eta2;
+  GetAngles(b, phi1, eta1, phi2, eta2);
   double dphi1 = fabs(TVector2::Phi_mpi_pi(phi1-b.met_phi()));
   double dphi2 = fabs(TVector2::Phi_mpi_pi(phi2-b.met_phi()));
   if(phi1 != -999 && phi2 != -999){
@@ -253,8 +285,8 @@ NamedFunc::ScalarType MinDeltaPhiLepMet(const Baby &b){
 }
 
 NamedFunc::ScalarType MaxDeltaPhiLepMet(const Baby &b){
-  double phi1, phi2;
-  GetAngles(b, phi1, phi2);
+  double phi1, eta1, phi2, eta2;
+  GetAngles(b, phi1, eta1, phi2, eta2);
   double dphi1 = fabs(TVector2::Phi_mpi_pi(phi1-b.met_phi()));
   double dphi2 = fabs(TVector2::Phi_mpi_pi(phi2-b.met_phi()));
   if(phi1 != -999 && phi2 != -999){
@@ -268,8 +300,8 @@ NamedFunc::ScalarType MaxDeltaPhiLepMet(const Baby &b){
 }
 
 NamedFunc::ScalarType MinDeltaPhiLepJet(const Baby &b){
-  double phi1, phi2;
-  GetAngles(b, phi1, phi2);
+  double phi1, eta1, phi2, eta2;
+  GetAngles(b, phi1, eta1, phi2, eta2);
   double minphi = -1.;
   for(size_t ijet = 0; ijet < b.jets_pt()->size(); ++ijet){
     if(!IsGoodJet(b,ijet)) continue;
@@ -291,8 +323,8 @@ NamedFunc::ScalarType MinDeltaPhiLepJet(const Baby &b){
 }
 
 NamedFunc::ScalarType MaxDeltaPhiLepJet(const Baby &b){
-  double phi1, phi2;
-  GetAngles(b, phi1, phi2);
+  double phi1, eta1, phi2, eta2;
+  GetAngles(b, phi1, eta1, phi2, eta2);
   double maxphi = -1.;
   for(size_t ijet = 0; ijet < b.jets_pt()->size(); ++ijet){
     if(!IsGoodJet(b,ijet)) continue;
@@ -335,4 +367,50 @@ NamedFunc::ScalarType MaxDeltaPhiMetJet(const Baby &b){
     }
   }
   return maxphi;
+}
+
+NamedFunc::ScalarType MinDeltaRLepJet(const Baby &b){
+  double phi1, eta1, phi2, eta2;
+  GetAngles(b, phi1, eta1, phi2, eta2);
+  double minr = -1.;
+  for(size_t ijet = 0; ijet < b.jets_pt()->size(); ++ijet){
+    if(!IsGoodJet(b,ijet)) continue;
+    double dr1 = hypot(TVector2::Phi_mpi_pi(phi1-b.jets_phi()->at(ijet)), eta2-eta1);
+    double dr2 = hypot(TVector2::Phi_mpi_pi(phi2-b.jets_phi()->at(ijet)), eta2-eta1);
+    double thisdr = -1;
+    if(phi1 != -999 && phi2 != -999){
+      thisdr = std::min(dr1, dr2);
+    }else if(phi1 != -999 && phi2 == -999){
+      thisdr = dr1;
+    }else if(phi1 == -999 && phi2 != -999){
+      thisdr = dr2;
+    }
+    if(minr < 0. || thisdr < minr){
+      minr = thisdr;
+    }
+  }
+  return minr;
+}
+
+NamedFunc::ScalarType MaxDeltaRLepJet(const Baby &b){
+  double phi1, eta1, phi2, eta2;
+  GetAngles(b, phi1, eta1, phi2, eta2);
+  double maxr = -1.;
+  for(size_t ijet = 0; ijet < b.jets_pt()->size(); ++ijet){
+    if(!IsGoodJet(b,ijet)) continue;
+    double dr1 = hypot(TVector2::Phi_mpi_pi(phi1-b.jets_phi()->at(ijet)), eta2-eta1);
+    double dr2 = hypot(TVector2::Phi_mpi_pi(phi2-b.jets_phi()->at(ijet)), eta2-eta1);
+    double thisdr = -1;
+    if(phi1 != -999 && phi2 != -999){
+      thisdr = std::max(dr1, dr2);
+    }else if(phi1 != -999 && phi2 == -999){
+      thisdr = dr1;
+    }else if(phi1 == -999 && phi2 != -999){
+      thisdr = dr2;
+    }
+    if(maxr < 0. || thisdr > maxr){
+      maxr = thisdr;
+    }
+  }
+  return maxr;
 }
