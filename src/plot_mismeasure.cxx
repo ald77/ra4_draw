da7edf7f0bd2004ce6ae4613e50302ae112b0f91
Adam Dishaw <aldishaw@gmail.com>
2016-07-14 15:14:59 -0700

    Add row and column normalized transfer matrices

--- src/plot_mismeasure.cxx
+++ src/plot_mismeasure.cxx
@@ -4,6 +4,7 @@
 #include <memory>
 
 #include "TStyle.h"
+#include "TColor.h"
 #include "TCanvas.h"
 #include "TChain.h"
 #include "TH1D.h"
@@ -15,6 +16,82 @@
 #include "baby_full.hpp"
 
 using namespace std;
+
+TH2D RowNorm(const TH2D &hin){
+  TH2D hout = hin;
+  hout.SetName((hout.GetName()+string("_rownorm")).c_str());
+  for(int irow = 1; irow <= hout.GetNbinsY(); ++irow){
+    double y = 0.;
+    for(int icol = 1; icol <= hout.GetNbinsX(); ++icol){
+      y += hout.GetBinContent(icol, irow);
+    }
+    y = 100./y;
+    for(int icol = 1; icol <= hout.GetNbinsY(); ++icol){
+      hout.SetBinContent(icol, irow, hout.GetBinContent(icol, irow)*y);
+      hout.SetBinError(icol, irow, hout.GetBinError(icol, irow)*y);
+    }
+  }
+  hout.SetMinimum(0.);
+  hout.SetMaximum(100.);
+  return hout;
+}
+
+TH2D ColNorm(const TH2D &hin){
+  TH2D hout = hin;
+  hout.SetName((hout.GetName()+string("_colnorm")).c_str());
+  for(int icol = 1; icol <= hout.GetNbinsY(); ++icol){
+    double y = 0.;
+    for(int irow = 1; irow <= hout.GetNbinsX(); ++irow){
+      y += hout.GetBinContent(icol, irow);
+    }
+    y = 100./y;
+    for(int irow = 1; irow <= hout.GetNbinsY(); ++irow){
+      hout.SetBinContent(icol, irow, hout.GetBinContent(icol, irow)*y);
+      hout.SetBinError(icol, irow, hout.GetBinError(icol, irow)*y);
+    }
+  }
+  hout.SetMinimum(0.);
+  hout.SetMaximum(100.);
+  return hout;
+}
+
+int GetRegionIndex(int nleps, float mt, float mj){
+  switch(nleps){
+  case 0:
+    if(mj<=250){
+      return 0;
+    }else if(mj<=400.){
+      return 1;
+    }else{
+      return 2;
+    }
+  case 1:
+    if(mj<=250){
+      return 0;
+    }else if(mj<=400.){
+      if(mt <= 140.){
+	return 3;
+      }else{
+	return 5;
+      }
+    }else{
+      if(mt <= 140.){
+	return 4;
+      }else{
+	return 6;
+      }
+    }
+  case 2:
+    if(mj<=250){
+      return 0;
+    }else if(mj<=400.){
+      return 7;
+    }else{
+      return 8;
+    }
+  default: return -1;
+  }
+}
 
 void Fill(bool pass, TH1D &h_pass, TH1D &h_total, double x, double w){
   if(pass) h_pass.Fill(x, w);
@@ -55,11 +132,43 @@ void MakePositive(TH1D &h){
   }
 }
 
+void SetAxisLabels(TAxis &a){
+  a.SetBinLabel(1, "0l, Low MJ");
+  a.SetBinLabel(2, "0l, High MJ");
+  a.SetBinLabel(3, "R1");
+  a.SetBinLabel(4, "R2");
+  a.SetBinLabel(5, "R3");
+  a.SetBinLabel(6, "R4");
+  a.SetBinLabel(7, "D3");
+  a.SetBinLabel(8, "D4");
+}
+
+void PrintTransfer(TH2D &h, bool region_labels = false){
+  if(region_labels){
+    SetAxisLabels(*h.GetXaxis());
+    SetAxisLabels(*h.GetYaxis());
+  }
+  Format(h, true);
+  TCanvas c;
+  h.Draw("col");
+  h.Draw("text same");
+  c.Print((string("plots/")+h.GetName()+".pdf").c_str());
+  TH2D rn = RowNorm(h);
+  rn.Draw("col");
+  rn.Draw("text same");
+  c.Print((string("plots/")+h.GetName()+"_rownorm.pdf").c_str());
+  TH2D cn = ColNorm(h);
+  cn.Draw("col");
+  cn.Draw("text same");
+  c.Print((string("plots/")+h.GetName()+"_colnorm.pdf").c_str());
+}
+
 void Print(TH2D &h, bool no_stats = false){
   Format(h, no_stats);
   h.SetTitle(("#rho="+to_string(h.GetCorrelationFactor())).c_str());
   TCanvas c;
-  h.Draw("scat");
+  h.Draw("col");
+  h.Draw("scat same");
   c.Print((string("plots/")+h.GetName()+".pdf").c_str());
 }
 
@@ -80,6 +189,13 @@ void Print(TH1D &pass, TH1D &total){
 int main(){
   gErrorIgnoreLevel = 6000;
   gStyle->SetOptStat("iouMRen");
+  double stops[2] = {0., 1.};
+  double red[2] = {1., 1.};
+  double green[2] = {1., 0.};
+  double blue[2] = {1., 0.};
+  gStyle->SetNumberContours(256);
+  gStyle->SetPaintTextFormat("4.2f");
+  TColor::CreateGradientColorTable(2, stops, red, green, blue, 256);
 
   double lumi = 2.6;
   int igood = 0;
@@ -127,6 +243,11 @@ int main(){
   TH2D h_2l_mm_mj_nolep_mj_nolep("h_2l_mm_mj_nolep_mj_nolep", ";Correct M_{J} (no lep) [GeV];Mismeasured M_{J} (no lep) [GeV]", 100, 0., 1000., 100, 0., 1000.);
   TH2D h_2l_mm_mj_lep_mj_lep("h_2l_mm_mj_lep_mj_lep", ";Correct M_{J} (with lep) [GeV];Mismeasured M_{J} (with lep) [GeV]", 100, 0., 1000., 100, 0., 1000.);
 
+  TH2D h_transfer_mm_lep("h_transfer_mm_lep", ";Correct Region;Mismeasured Region", 8, 0.5, 8.5, 8, 0.5, 8.5);
+  TH2D h_transfer_all_lep("h_transfer_all_lep", ";Correct Region;Mismeasured Region", 8, 0.5, 8.5, 8, 0.5, 8.5);
+  TH2D h_transfer_mm_nolep("h_transfer_mm_nolep", ";Correct Region;Mismeasured Region", 8, 0.5, 8.5, 8, 0.5, 8.5);
+  TH2D h_transfer_all_nolep("h_transfer_all_nolep", ";Correct Region;Mismeasured Region", 8, 0.5, 8.5, 8, 0.5, 8.5);
+
   TH1D h_num_1l_mt("h_num_1l_mt", "Numerator;m_{T} [GeV];Entries", 20, 0., 1000.);
   TH1D h_den_1l_mt("h_den_1l_mt", "Denominator;m_{T} [GeV];Entries", 20, 0., 1000.);
   TH1D h_num_1l_mj_lep("h_num_1l_mj_lep", "Numerator;M_{J} (with lep) [GeV];Entries", 20, 0., 1000.);
@@ -166,11 +287,33 @@ int main(){
       double w = 10.*lumi*b->weight();
 
       bool mm = b->mm()->at(ibad);
+
+      if((b->mm_ht()->at(igood)>500 || b->mm_ht()->at(ibad)>500)
+	 && (b->mm_met()->at(igood)>200 || b->mm_met()->at(ibad)>200)
+	 && (b->mm_njets()->at(igood)>=5 || b->mm_njets()->at(ibad)>=5)
+	 && (b->mm_nbm()->at(igood)>=1 || b->mm_nbm()->at(ibad)>=1)){
+	if(mm){
+	  h_transfer_mm_lep.Fill(GetRegionIndex(b->mm_nleps()->at(igood), b->mm_mt()->at(igood), b->mm_mj14_lep()->at(igood)),
+			      GetRegionIndex(b->mm_nleps()->at(ibad), b->mm_mt()->at(ibad), b->mm_mj14_lep()->at(ibad)),
+			      w);
+	  h_transfer_mm_nolep.Fill(GetRegionIndex(b->mm_nleps()->at(igood), b->mm_mt()->at(igood), b->mm_mj14_nolep()->at(igood)),
+				GetRegionIndex(b->mm_nleps()->at(ibad), b->mm_mt()->at(ibad), b->mm_mj14_nolep()->at(ibad)),
+				w);
+	}
+	h_transfer_all_lep.Fill(GetRegionIndex(b->mm_nleps()->at(igood), b->mm_mt()->at(igood), b->mm_mj14_lep()->at(igood)),
+			    GetRegionIndex(b->mm_nleps()->at(ibad), b->mm_mt()->at(ibad), b->mm_mj14_lep()->at(ibad)),
+			    w);
+	h_transfer_all_nolep.Fill(GetRegionIndex(b->mm_nleps()->at(igood), b->mm_mt()->at(igood), b->mm_mj14_nolep()->at(igood)),
+			      GetRegionIndex(b->mm_nleps()->at(ibad), b->mm_mt()->at(ibad), b->mm_mj14_nolep()->at(ibad)),
+			      w);
+      }
+
       if(mm
 	 && (b->mm_nleps()->at(igood)>0 || b->mm_nleps()->at(ibad)>0)
 	 && (b->mm_ht()->at(igood)>500 || b->mm_ht()->at(ibad)>500)
 	 && (b->mm_met()->at(igood)>200 || b->mm_met()->at(ibad)>200)
 	 && (b->mm_njets()->at(igood)>=5 || b->mm_njets()->at(ibad)>=5)){
+
 	if(b->mm_nleps()->at(ibad)==1){
 	  h_1l_mm_lep_pt.Fill(b->mm_lep_pt()->at(ibad), w);
 	  h_1l_mt_mj_lep.Fill(b->mm_mj14_lep()->at(igood), b->mm_mt()->at(igood), w);
@@ -251,4 +394,9 @@ int main(){
   Print(h_2l_mm_mt_mt);
   Print(h_2l_mm_mj_nolep_mj_nolep);
   Print(h_2l_mm_mj_lep_mj_lep);
+
+  PrintTransfer(h_transfer_mm_lep, true);
+  PrintTransfer(h_transfer_mm_nolep, true);
+  PrintTransfer(h_transfer_all_lep, true);
+  PrintTransfer(h_transfer_all_nolep, true);
 }
