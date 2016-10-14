0c79dc569e9e81524d3b2e45c48fb5b9d195b039
Ana Ovcharova <akovcharova@gmail.com>
2016-10-14 11:18:56 -0700

    Pie charts with latest category split

--- src/ra4/plot_pie.cxx
+++ src/ra4/plot_pie.cxx
@@ -22,6 +22,16 @@ namespace{
 
 using namespace std;
 
+NamedFunc offshellw("offshellw",[](const Baby &b) -> NamedFunc::ScalarType{
+    for (unsigned i(0); i<b.mc_pt()->size(); i++){
+      if (abs(b.mc_id()->at(i))!=24) continue;
+      if (b.mc_mass()->at(i) > 140.) {
+        return 1;
+      }
+    }
+    return 0;
+  });
+
 int main(){
   gErrorIgnoreLevel=6000; // Turns off ROOT errors due to missing branches
 
@@ -35,99 +45,126 @@ int main(){
   if(Contains(hostname, "cms") || Contains(hostname, "compute-"))
     bfolder = "/net/cms2"; // In laptops, you can't create a /net folder
 
-  string foldermc(bfolder+"/cms2r0/babymaker/babies/2016_08_10/mc/merged_mcbase_stdnj5/");
+  string foldermc(bfolder+"/cms2r0/babymaker/babies/2016_08_10/mc/merged_mcbase_met100_stdnj5/");
   //if(do_met150) foldermc = (bfolder+"/cms2r0/babymaker/babies/2016_06_14/mc/merged_met150/");
   Palette colors("txt/colors.txt", "default");
 
+  string ntupletag = "";
+  set<string> allfiles = {foldermc+"*_TTJets*Lept*"+ntupletag+"*.root", foldermc+"*_TTJets_HT*"+ntupletag+"*.root",
+         foldermc+"*_WJetsToLNu*"+ntupletag+"*.root",foldermc+"*_ST_*"+ntupletag+"*.root",
+         foldermc+"*_TTW*"+ntupletag+"*.root",foldermc+"*_TTZ*"+ntupletag+"*.root",
+         foldermc+"*_TTGJets*"+ntupletag+"*.root",foldermc+"*_TTTT*"+ntupletag+"*.root",
+         foldermc+"*QCD_HT*Inf_Tune*"+ntupletag+"*.root", foldermc+"*QCD_HT*0_Tune*"+ntupletag+"*.root",
+         foldermc+"*DYJetsToLL*"+ntupletag+"*.root",
+         foldermc+"*_ZJet*"+ntupletag+"*.root",foldermc+"*_ttHJetTobb*"+ntupletag+"*.root",
+         foldermc+"*_WH_HToBB*"+ntupletag+"*.root",foldermc+"*_ZH_HToBB*"+ntupletag+"*.root",
+         foldermc+"*_WWTo*"+ntupletag+"*.root",foldermc+"*_WZ*"+ntupletag+"*.root",foldermc+"*_ZZ_*"+ntupletag+"*.root"
+       };
+
   // Cuts in baseline speed up the yield finding
-  string baseline = "mj14>250 && nleps>=1 && ht>500 && met>150 && pass && njets>=5 && weight<1"; // Excluding one QCD event
-  //if(do_met150)  baseline = "mj14>250 && nleps>=1 && ht>500 && met>150 && pass && njets>=5";
+  string baseline = "pass && stitch && mj14>250 && nleps>=1 && st>500 && met>100 && njets>=5 && weight<1"; // Excluding one QCD event
 
-  auto proc_tt1l = Process::MakeShared<Baby_full>("t#bar{t} (l)", Process::Type::background, colors("tt_1l"),
-    {foldermc+"*_TTJets*SingleLept*.root", foldermc+"*_TTJets_HT*.root"},
-    baseline+" && stitch && ntruleps==1");
-  auto proc_tt2l = Process::MakeShared<Baby_full>("t#bar{t} (ll)", Process::Type::background, colors("tt_2l"),
-    {foldermc+"*_TTJets*DiLept*.root", foldermc+"*_TTJets_HT*.root"},
-    baseline+" && stitch && ntruleps==2 && ntrutaush==0");
-  auto proc_ttltau = Process::MakeShared<Baby_full>("t#bar{t} (#tau_{h}l)", Process::Type::background, colors("tt_ltau"),
-    {foldermc+"*_TTJets*DiLept*.root", foldermc+"*_TTJets_HT*.root"},
-    baseline+" && stitch && ntruleps==2 && ntrutaush>=1");
-  auto proc_wjets = Process::MakeShared<Baby_full>("W+jets", Process::Type::background, colors("wjets"),
-    {foldermc+"*_WJetsToLNu*.root"}, baseline+" && stitch");
-  auto proc_single_t = Process::MakeShared<Baby_full>("Single t", Process::Type::background, colors("single_t"),
-    {foldermc+"*_ST_*.root"}, baseline);
-  auto proc_ttv = Process::MakeShared<Baby_full>("t#bar{t}V", Process::Type::background, colors("ttv"),
-    {foldermc+"*_TTWJets*.root", foldermc+"*_TTZ*.root"}, baseline);
-  auto proc_other = Process::MakeShared<Baby_full>("Other", Process::Type::background, colors("other"),
-    {foldermc+"*DYJetsToLL*.root",foldermc+"*QCD_HT*0_Tune*.root",foldermc+"*QCD_HT*Inf_Tune*.root",
-        foldermc+"*_ZJet*.root",foldermc+"*_ttHJetTobb*.root",
-        foldermc+"*_TTGJets*.root",foldermc+"*_TTTT*.root",
-        foldermc+"*_WH_HToBB*.root",foldermc+"*_ZH_HToBB*.root",
-        foldermc+"*_WWTo*.root",foldermc+"*_WZ*.root",foldermc+"*_ZZ_*.root"},
-    baseline+" && stitch");
+  map<string, vector<shared_ptr<Process> > > procs;
+  // procs["procs"] = vector<shared_ptr<Process> >();
+  // procs["procs"].push_back(Process::MakeShared<Baby_full>("t#bar{t} (l)", Process::Type::background, colors("tt_1l"),
+  //   {foldermc+"*_TTJets*SingleLept*.root", foldermc+"*_TTJets_HT*.root"},
+  //   baseline+" && ntruleps==1"));
+  // procs["procs"].push_back(Process::MakeShared<Baby_full>("t#bar{t} (ll)", Process::Type::background, colors("tt_2l"),
+  //   {foldermc+"*_TTJets*DiLept*.root", foldermc+"*_TTJets_HT*.root"},
+  //   baseline+" && ntruleps==2 && ntrutaush==0"));
+  // procs["procs"].push_back(Process::MakeShared<Baby_full>("t#bar{t} (#tau_{h}l)", Process::Type::background, colors("tt_ltau"),
+  //   {foldermc+"*_TTJets*DiLept*.root", foldermc+"*_TTJets_HT*.root"},
+  //   baseline+" && ntruleps==2 && ntrutaush>=1"));
+  // procs["procs"].push_back(Process::MakeShared<Baby_full>("W+jets", Process::Type::background, colors("wjets"),
+  //   {foldermc+"*_WJetsToLNu*.root"}, baseline));
+  // procs["procs"].push_back(Process::MakeShared<Baby_full>("Single t", Process::Type::background, colors("single_t"),
+  //   {foldermc+"*_ST_*.root"}, baseline));
+  // procs["procs"].push_back(Process::MakeShared<Baby_full>("t#bar{t}Z", Process::Type::background, kOrange+7,
+  //   {foldermc+"*_TTZ*.root"}, baseline));
+  // procs["procs"].push_back(Process::MakeShared<Baby_full>("t#bar{t}W", Process::Type::background, colors("ttv"),
+  //   {foldermc+"*_TTW*.root"}, baseline));
+  // procs["procs"].push_back(Process::MakeShared<Baby_full>("QCD", Process::Type::background, colors("other"),
+  // {foldermc+"*QCD_HT*0_Tune*.root",
+  //   foldermc+"*QCD_HT*Inf_Tune*.root"},
+  //   baseline));
+  // procs["procs"].push_back(Process::MakeShared<Baby_full>("tt#gamma", Process::Type::background, kMagenta+3,
+  //   {foldermc+"*_TTGJets*.root"}, baseline));
+  // procs["procs"].push_back(Process::MakeShared<Baby_full>("tttt", Process::Type::background, kBlue+2,
+  //   {foldermc+"*_TTTT*.root"}, baseline));
+  // procs["procs"].push_back(Process::MakeShared<Baby_full>("Other", Process::Type::background, kPink-2,
+  //   {foldermc+"*DYJetsToLL*.root",
+  //   foldermc+"*_ZJet*.root",
+  //   foldermc+"*_ttHJetTobb*.root",
+  //   foldermc+"*_WH_HToBB*.root",
+  //   foldermc+"*_ZH_HToBB*.root",
+  //   foldermc+"*_WWTo*.root",
+  //   foldermc+"*_WZ*.root",
+  //   foldermc+"*_ZZ_*.root"},
+  //   baseline));
 
-  // auto proc_tt1l = Process::MakeShared<Baby_full>("t#bar{t} (1l", Process::Type::background, colors("tt_1l"),
-  //   {foldermc+"*_TTJets_Tune*.root"},
-  //   baseline+" && ntruleps==1");
-  // auto proc_tt2l = Process::MakeShared<Baby_full>("t#bar{t} 2l", Process::Type::background, colors("tt_2l"),
-  //   {foldermc+"*_TTJets_Tune**.root"},
-  //   baseline+" && ntruleps==2");
+  // procs["goodbad"] = vector<shared_ptr<Process> >();
 
+  // procs["goodbad"].push_back(Process::MakeShared<Baby_full>("2l", Process::Type::background, kCyan-3,
+  //   allfiles, baseline && "ntruleps>=2"));
+  // procs["goodbad"].push_back(Process::MakeShared<Baby_full>("0-1l m_{T}^{tru}>140", Process::Type::background, kGreen-3,
+  //   allfiles, baseline && "ntruleps<=1 && mt_tru>140"));
+  // procs["goodbad"].push_back(Process::MakeShared<Baby_full>("0-1l m_{T}^{tru}<=140", Process::Type::background, kRed,
+  //   allfiles, baseline && "ntruleps<=1 && mt_tru<=140"));
 
+  procs["cats"] = vector<shared_ptr<Process> >();
 
-  vector<shared_ptr<Process> > all_procs = {proc_tt1l, proc_tt2l, proc_ttltau, proc_wjets, proc_single_t, proc_ttv, proc_other};
-  //vector<shared_ptr<Process> > all_procs = {proc_tt1l, proc_tt2l};
-
-  ////// MET cuts
-  TString c_vlowmet = "met>150 && met<=200";
-  TString c_lowmet  = "met>200 && met<=350";
-  TString c_midmet  = "met>350 && met<=500";
-  TString c_higmet  = "met>500";
-  vector<TString> metcuts({c_vlowmet, c_lowmet, c_midmet, c_higmet});
-  //  if(do_met150) metcuts = vector<TString>({c_vlowmet});
-
-  ////// Nb cuts
-  TString c_lownb = "nbm==1";
-  TString c_midnb = "nbm==2";
-  TString c_hignb = "nbm>=3";
-  vector<TString> nbcuts({c_lownb, c_midnb, c_hignb});
-  //vector<TString> nbcuts({"nbm>=2"});
-
-  ////// Njets cuts
-  TString c_lownj = "njets>=6 && njets<=8";
-  TString c_hignj = "njets>=9";
-  TString c_nj5   = "njets==5";
-  vector<TString> njcuts({c_nj5, c_lownj, c_hignj});
-  //vector<TString> njcuts({"njets>=6"});
-
-  ////// mT cuts
-  TString c_lowmt = "mt<=140";
-  TString c_higmt = "mt>140";
-  vector<TString> mtcuts({c_lowmt, c_higmt});
-
-  // Adding nleps==1 cuts
-  vector<TString> cuts;
-  for(size_t imet=0; imet<metcuts.size(); imet++)
-    for(size_t inb=0; inb<nbcuts.size(); inb++)
-      for(size_t inj=0; inj<njcuts.size(); inj++)
-	for(size_t imt=0; imt<mtcuts.size(); imt++){
-	  cuts.push_back("nleps==1 && nveto==0 && "+metcuts[imet]+"&&"+nbcuts[inb]+"&&"+njcuts[inj]+"&&"+mtcuts[imt]);
-	  cuts.push_back("nleps==1 && nveto==1 && "+metcuts[imet]+"&&"+nbcuts[inb]+"&&"+njcuts[inj]+"&&"+mtcuts[imt]);
-	}
-
-  // Adding nleps==2 cuts
-  njcuts = vector<TString>({"njets>=5 && njets<=7", "njets>=8"});
-  for(size_t imet=0; imet<metcuts.size(); imet++)
-    for(size_t inj=0; inj<njcuts.size(); inj++)
-      cuts.push_back("nleps==2 && "+metcuts[imet]+"&&"+njcuts[inj]);
- 
-  vector<TableRow> table_cuts;
-  for(size_t icut=0; icut<cuts.size(); icut++){
-    table_cuts.push_back(TableRow("$"+CodeToLatex(cuts[icut].Data())+"$", cuts[icut].Data()));
-  }
+  procs["cats"].push_back(Process::MakeShared<Baby_full>("#geq 2l", 
+    Process::Type::background, kCyan-3,
+    allfiles, baseline && "ntruleps>=2"));
+  procs["cats"].push_back(Process::MakeShared<Baby_full>("#leq 1l, #geq 2#nu", 
+    Process::Type::background, kAzure-2,
+    allfiles, baseline && "ntruleps<=1 && (type==5000 || type==13000 || type==15000 || type==16000)"));
+  procs["cats"].push_back(Process::MakeShared<Baby_full>("0-1l m_{T}^{tru}#leq 140", 
+    Process::Type::background, kRed-4,
+    allfiles, baseline && "ntruleps<=1 && mt_tru<=140 && !(type==5000 || type==13000 || type==15000 || type==16000)"));
+  procs["cats"].push_back(Process::MakeShared<Baby_full>("1l m_{T}^{tru}>140, no W#lower[-.1]{*}", 
+    Process::Type::background, kOrange,
+    allfiles, baseline && "ntruleps<=1 && mt_tru>140 && !(type==5000 || type==13000 || type==15000 || type==16000)" && offshellw==0.));
+  procs["cats"].push_back(Process::MakeShared<Baby_full>("1l m_{T}^{tru}>140, W#lower[-.1]{*}", 
+    Process::Type::background, kGreen-3,
+    allfiles, baseline && "ntruleps<=1 && mt_tru>140 && !(type==5000 || type==13000 || type==15000 || type==16000)" && offshellw>0.));
 
   PlotMaker pm;
-  pm.Push<Table>("chart",  table_cuts, all_procs, true, true, true);
+
+  vector<TString> metcuts;
+  metcuts.push_back("met>100");
+  metcuts.push_back("met>100 && met<=150");
+  metcuts.push_back("met>150 && met<=200");
+  metcuts.push_back("met>200 && met<=350");
+  metcuts.push_back("met>350 && met<=500");
+  metcuts.push_back("met>500");
+
+  vector<TString> nbcuts;
+  nbcuts.push_back("nbm==0");
+  nbcuts.push_back("nbm==1");
+  nbcuts.push_back("nbm>=1");
+  nbcuts.push_back("nbm>=2");
+
+  vector<TString> njcuts;
+  njcuts.push_back("njets==5");
+  njcuts.push_back("njets>=5");
+  njcuts.push_back("njets>=6");
+  njcuts.push_back("njets>=6 && njets<=8");
+  njcuts.push_back("njets>=9");
+  
+  vector<TString> mtcuts({"mt<=140", "mt>140"});
+  
+  // Adding nleps==1 cuts
+  vector<TString> cuts;
+  vector<TableRow> table_cuts;
+
+  for(auto &imet: metcuts) for(auto &inb: nbcuts) for(auto &inj: njcuts) for(auto &imt: mtcuts)
+    cuts.push_back("nleps==1 && nveto==0 && "+imet+"&&"+inb+"&&"+inj+"&&"+imt);
+  for(size_t icut=0; icut<cuts.size(); icut++)
+    table_cuts.push_back(TableRow("$"+CodeToLatex(cuts[icut].Data())+"$", cuts[icut].Data()));  
+  for(auto &ipr: procs) 
+    pm.Push<Table>("chart_"+ipr.first,  table_cuts, ipr.second, true, true, true);
+
   pm.min_print_ = true;
   pm.MakePlots(40.);
 
