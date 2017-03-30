18840ffefdecbab039a4045cd7d825f7c4acecb2
Manuel Franco Sevilla <manuelf@physics.ucsb.edu>
2017-03-29 19:31:05 -0700

    Added TCHiHW and TChiHZ diagram

--- python/draw_TChiHH.py
+++ python/draw_TChiHH.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 from pyfeyn.user import *
 from math import sin,cos,tan
 import pyx 
@@ -5,8 +7,27 @@ import pyx
 # paint styles
 # https://pyfeyn.hepforge.org/doc/pyfeyn.paint-module.html
 
-chi10=r"$\tilde{\chi}^0_1$"
-goldstino=r"$\tilde{G}$"
+model = "TChiHH"
+model = "TChiHW"
+model = "TChiHZ"
+
+
+### Defining labels
+lspTop_s=r"$\tilde{\chi}^0_1$"
+lspBottom_s=r"$\tilde{\chi}^0_1$"
+lsp=r"$\tilde{G}$"
+dauTop_s = "h"
+dauBottom_s = "h"
+
+if model == "TChiHZ":
+    dauBottom_s = "Z"
+
+if model == "TChiHW":
+    lspTop_s=r"$\tilde{\chi}^0_2$"
+    lspBottom_s=r"$\tilde{\chi}^\pm_1$"
+    lsp=r"$\tilde{\chi}^0_1$"
+    dauBottom_s = r"W$^\pm$"
+
 
 susycolor = RED
 # susycolor = BLACK
@@ -42,32 +63,34 @@ vtx_both = Vertex(dx_1+dx_h, -(dx_1*tan(ang_1)+dx_h*tan(ang_h)))
 vtx_botg = Vertex(dx_1+dx_g, -(dx_1*tan(ang_1)+dx_g*tan(ang_g)))
 
 
-chi10_top = Vector(blob, vtx_top).setAmplitude(0.1).setFrequency(0.4)
-chi10_topin = Line(blob, vtx_top).addStyle(linewidth).addStyle(susycolor)
-# chi10_top = Gaugino(blob, vtx_top)
-# chi10_top.set3D()
-chi10_top.addLabel(chi10)
-chi10_top.addStyle(linewidth).addStyle(susycolor)
-h_top = Higgs(vtx_top, vtx_toph).addLabel("$h$",displace=0.05, pos = 1.3)
-# h_top = Higgs(vtx_top, vtx_toph).addLabel("$h$",angle=90, pos = 1.3)
-h_top.addStyle(linewidth)
-g_top = Ghost(vtx_top, vtx_topg).addLabel(goldstino,displace=0.005, pos = 1.2)
-g_top.addStyle(linewidth).addStyle(susycolor)
+nlsp_top = Vector(blob, vtx_top).setAmplitude(0.1).setFrequency(0.4)
+nlsp_topin = Line(blob, vtx_top).addStyle(linewidth).addStyle(susycolor)
+# nlsp_top = Gaugino(blob, vtx_top)
+# nlsp_top.set3D()
+nlsp_top.addLabel(lspTop_s)
+nlsp_top.addStyle(linewidth).addStyle(susycolor)
+dau_top = Higgs(vtx_top, vtx_toph).addLabel(dauTop_s,displace=0.05, pos = 1.3)
+# dau_top = Higgs(vtx_top, vtx_toph).addLabel(dauTop_s,angle=90, pos = 1.3)
+dau_top.addStyle(linewidth)
+lsp_top = Ghost(vtx_top, vtx_topg).addLabel(lsp,displace=0.005, pos = 1.2)
+lsp_top.addStyle(linewidth).addStyle(susycolor)
 
-chi10_bot = Vector(blob, vtx_bot).setAmplitude(0.1).setFrequency(0.4)
-chi10_botin = Line(blob, vtx_bot).addStyle(linewidth).addStyle(susycolor)
-# chi10_bot = Gaugino(blob, vtx_bot)
-# chi10_bot.set3D()
-chi10_bot.addLabel(chi10,displace=.34)
-chi10_bot.addStyle(linewidth).addStyle(susycolor)
-chi10_bot.invert()
+nlsp_bot = Vector(blob, vtx_bot).setAmplitude(0.1).setFrequency(0.4)
+nlsp_botin = Line(blob, vtx_bot).addStyle(linewidth).addStyle(susycolor)
+# nlsp_bot = Gaugino(blob, vtx_bot)
+# nlsp_bot.set3D()
+nlsp_bot.addLabel(lspBottom_s,displace=.34)
+nlsp_bot.addStyle(linewidth).addStyle(susycolor)
+nlsp_bot.invert()
 
-g_bot = Ghost(vtx_bot, vtx_botg).addLabel(goldstino,displace=0.01, pos = 1.2)
-g_bot.addStyle(linewidth).addStyle(susycolor)
-h_bot = Higgs(vtx_bot, vtx_both).addLabel("$h$",displace=0., pos = 1.3)
-h_bot.addStyle(linewidth)
+lsp_bot = Ghost(vtx_bot, vtx_botg).addLabel(lsp,displace=0.01, pos = 1.2)
+lsp_bot.addStyle(linewidth).addStyle(susycolor)
+if model == "TChiHH": dau_bot = Higgs(vtx_bot, vtx_both).addLabel(dauBottom_s,displace=0., pos = 1.3)
+else: dau_bot = Vector(vtx_bot, vtx_both).addLabel(dauBottom_s,displace=0., pos = 1.3).setAmplitude(0.05).setFrequency(0.3)
+dau_bot.addStyle(linewidth)
 
+plotname = model+"_diag.pdf"
+fd.draw(plotname)
 
-fd.draw("TChiHH_diag.pdf")
-
+print "\n open "+plotname+"\n"
 # os.system ("convert %s_feyn.pdf -transparent white %s_feyn.png" % (name, name))
